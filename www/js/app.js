e52598abf94248fe4a94afa55ba7c27dad705703
elyas_yavari <elyasyavari@gmail.com>
2016-04-02 19:20:33 +0430

    admob

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,45 @@ var app = {
         app.initPushwoosh();
         console.log("salam");
         
+         function initAds() {
+          if (admob) {
+            var adPublisherIds = {
+              ios : {
+                banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+                interstitial : "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII"
+              },
+              android : {
+                banner: 'ca-app-pub-9325324970346208/5891514772',
+                interstitial: 'ca-app-pub-9325324970346208/8844981178'
+              }
+            };
+
+            var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+
+            admob.setOptions({
+              publisherId:      admobid.banner,
+              interstitialAdId: admobid.interstitial,
+
+            });
+
+            registerAdEvents();
+
+          } else {
+            alert('AdMobAds plugin not ready');
+          }
+        }
+        
+        initAds();
+
+      // display a banner at startup
+      admob.createBannerView();
+
+      // request an interstitial
+      admob.requestInterstitialAd();
+        
+        
+        
+        
         //Admob
         var admobid = {};
         if( /(android)/i.test(navigator.userAgent) ) { // for android
