cbd9c92ee36202ab2d4cfec3b32b5a7da94c1f8c
elyas_yavari <elyasyavari@gmail.com>
2016-04-04 13:22:24 +0430

    ok

--- www/js/app.js
+++ www/js/app.js
@@ -70,12 +70,12 @@ var app = {
         var admobid = {};
         if( /(android)/i.test(navigator.userAgent) ) { 
             admobid = { // for Android
-                banner: 'ca-app-pub-6869992474017983/9375997553',
+                banner: 'ca-app-pub-9588717711097708/1137818873',
                 interstitial: 'ca-app-pub-6869992474017983/1657046752'
             };
         } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
             admobid = { // for iOS
-                banner: 'ca-app-pub-6869992474017983/4806197152',
+                banner: 'ca-app-pub-9588717711097708/4091285279',
                 interstitial: 'ca-app-pub-6869992474017983/7563979554'
             };
         } else {
